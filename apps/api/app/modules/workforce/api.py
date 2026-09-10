6b5af5e100bb3c735283c565339f933f0c7edaef
sabarihomelab <homelabsabari@gmail.com>
2026-09-10 19:37:09 +0530

    Aggregate Workforce API routers

---
+++ apps/api/app/modules/workforce/api.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from app.modules.workforce.management_router import router as management_router
+from app.modules.workforce.router import router as core_router
+
+router = APIRouter()
+router.include_router(core_router)
+router.include_router(management_router)
