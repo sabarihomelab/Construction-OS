5f695038f0527956c6599ea429008498baebad88
sabarihomelab <homelabsabari@gmail.com>
2026-09-15 19:48:18 +0530

    add DPR-owned observations to detail

--- apps/web/app/field/dpr/[reportId]/page.tsx
+++ apps/web/app/field/dpr/[reportId]/page.tsx
@@ -1,5 +1,6 @@
 import DPRFieldExtensions from "../../../dpr-field-extensions";
 import DPRGovernancePanel from "../../../dpr-governance-panel";
+import DPRReportOwnedSections from "../../../dpr-report-owned-sections";
 import DPRVoidControl from "../../../dpr-void-control";
 import DPRWorkspace from "../../../dpr-workspace";
 
@@ -15,6 +16,7 @@ export default async function DPRDetailPage({
   return <>
     <DPRWorkspace initialProjectId={project} initialReportId={reportId} />
     {project && <main className="workspace-shell">
+      <DPRReportOwnedSections projectId={project} reportId={reportId} />
       <DPRFieldExtensions projectId={project} reportId={reportId} />
       <DPRGovernancePanel projectId={project} reportId={reportId} />
       <DPRVoidControl projectId={project} reportId={reportId} />
