e4ea9f04054a91c7a983549ca783bbf92222bed6
sabarihomelab <homelabsabari@gmail.com>
2026-09-16 22:59:28 +0530

    register client receivable api

--- apps/api/app/modules/financials/api.py
+++ apps/api/app/modules/financials/api.py
@@ -8,6 +8,7 @@ from app.modules.financials.feed_router import router as feed_router
 from app.modules.financials.mapping_router import router as mapping_router
 from app.modules.financials.material_feed_router import router as material_feed_router
 from app.modules.financials.payables_router import router as payables_router
+from app.modules.financials.receivable_router import router as receivable_router
 from app.modules.financials.router import router as core_router
 from app.modules.financials.subcontract_feed_router import router as subcontract_feed_router
 
@@ -22,3 +23,4 @@ router.include_router(commitment_router)
 router.include_router(commercial_control_router)
 router.include_router(budget_control_router)
 router.include_router(payables_router)
+router.include_router(receivable_router)
